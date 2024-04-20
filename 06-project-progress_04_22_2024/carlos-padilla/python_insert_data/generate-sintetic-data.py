085c41eb12d5fdb28130be5dbc3b1c710a80652e
Ignacio Padilla <enigmak9@protonmail.com>
2024-04-20 00:24:18 -0600

    sinthetic data updated

---
+++ 06-project-progress_04_22_2024/carlos-padilla/python_insert_data/generate-sintetic-data.py
@@ -0,0 +1,21 @@
+def generateSintheticData(numSampling=1000, initialSoC=95):
+    np.random.seed(42)  # Para reproducibilidad
+    initialTime = np.random.uniform(0, 100, numSampling)
+    Durations = np.random.uniform(1, 10, numSampling)
+    Powers = np.random.uniform(1, 3, numSampling)
+    PriorityTs = np.random.uniform(0, 1, numSampling)
+    PriorityEs = np.random.uniform(0, 1, numSampling)
+
+    dfEntrenamiento = pd.DataFrame({
+        'HoraInicio': initialTime,
+        'Duracion': Durations,
+        'Potencia': Powers,
+        'PriorityT': PriorityTs,
+        'PriorityE': PriorityEs
+    })
+
+# Generate sinthetic data to train model
+dfTraining = generateSintheticData(numSampling=1000, initialSoC=95)
+
+# check data
+print(dfTraining.head())
