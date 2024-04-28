52acf56bf358f021cc3cb6ad29c609b88efd98aa
Ignacio Padilla <enigmak9@protonmail.com>
2024-04-27 21:50:29 -0600

    updated reward function code

--- 07-progress_04_30_2024/reward-function.py
+++ 07-progress_04_30_2024/reward-function.py
@@ -1,1 +1,16 @@
-meow
+'''
+como vamos a generar y definir los datos de la función de recompensa de Tavis
+'''
+S_j = 
+# vamos a usar el código de jorge para generar s_j Red002.ipynb
+P_j = 
+t_j_E = 
+t_j_R = 
+sigma = 
+P_j_D =
+d_j =
+g_k =
+
+'''
+Como vamos a generar y definir los datos de la función de recompensa de Marlene
+'''
